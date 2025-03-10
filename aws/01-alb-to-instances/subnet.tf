84380147e6dd1db7fc67b673e44f2fce876203f0
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-03-10 23:42:51 +0900

    feat: add basic files

---
+++ aws/01-alb-to-instances/subnet.tf
@@ -0,0 +1,19 @@
+resource aws_subnet subnet1 {
+	vpc_id = aws_vpc.vpc.id
+	availability_zone = ""
+}
+
+resource aws_subnet subnet2 {
+	vpc_id = aws_vpc.vpc.id
+	availability_zone = ""
+}
+
+resource aws_subnet subnet3 {
+	vpc_id = aws_vpc.vpc.id
+	availability_zone = ""
+}
+
+resource aws_subnet subnet4 {
+	vpc_id = aws_vpc.vpc.id
+	availability_zone = ""
+}
