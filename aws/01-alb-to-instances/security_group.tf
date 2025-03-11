26e9fd6c9aeb5bff1367f143a09ef865917b9901
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-03-12 00:20:23 +0900

    feat: format and update security group

--- aws/01-alb-to-instances/security_group.tf
+++ aws/01-alb-to-instances/security_group.tf
@@ -1,14 +1,41 @@
-resource aws_security_group security_group {
-	vpc_id = aws_vpc.vpc.id
-	name = "from-alb-to-anywhere"
-	description = "Allow all egress, only allow ingress from ALB"
+resource "aws_security_group" "instance_security_group" {
+  vpc_id      = aws_vpc.vpc.id
+  name        = "from-alb-to-anywhere"
+  description = "Allow all egress, only allow ingress from ALB"
 
-	# to anywhere
-	egress {
-		from_port = 0
-		to_port = 0
-		protocol = "-1"
-	}
+  # to anywhere
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+  }
 
-	ingress = []
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    security_groups = [
+    ]
+  }
+}
+
+resource "aws_security_group" "alb_security_group" {
+  vpc_id      = aws_vpc.vpc.id
+  name        = "from-internet-gateway-to-instances"
+  description = "Ingress from internet gateway, egress to instance security group"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+  }
+
+  egress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    security_groups = [
+      aws_security_group.instance_security_group.id
+    ]
+  }
 }
