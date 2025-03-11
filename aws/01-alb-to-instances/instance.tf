de4b06dbfed471d3ed70bab243a671c2800865e7
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-03-11 23:50:03 +0900

    fix: add missing subnet id for instances

--- aws/01-alb-to-instances/instance.tf
+++ aws/01-alb-to-instances/instance.tf
@@ -5,7 +5,8 @@ locals {
 
 resource "aws_instance" "instance1" {
   ami           = local.ami
-  private_ip    = "192.168.0.0"
+  subnet_id     = aws_subnet.subnet1.id
+  private_ip    = "192.168.0.1"
   instance_type = local.type
   tags = {
     Name = "project-01-subnet-01-instance-01"
@@ -14,6 +15,7 @@ resource "aws_instance" "instance1" {
 
 resource "aws_instance" "instance2" {
   ami           = local.ami
+  subnet_id     = aws_subnet.subnet2.id
   private_ip    = "192.168.1.1"
   instance_type = local.type
   tags = {
@@ -23,6 +25,7 @@ resource "aws_instance" "instance2" {
 
 resource "aws_instance" "instance3" {
   ami           = local.ami
+  subnet_id     = aws_subnet.subnet3.id
   private_ip    = "192.168.2.1"
   instance_type = local.type
   tags = {
