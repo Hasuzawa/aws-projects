c9abdc61fb5b3ebef46312d92c4d93d79032b795
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-03-11 22:27:06 +0900

    feat: create vpc & 3 subnets in apne

--- aws/01-alb-to-instances/subnet.tf
+++ aws/01-alb-to-instances/subnet.tf
@@ -1,19 +1,26 @@
 resource aws_subnet subnet1 {
 	vpc_id = aws_vpc.vpc.id
-	availability_zone = ""
+	availability_zone = "ap-northeast-1a"
+	cidr_block = "192.168.0.0/24"
+	tags = {
+		Name = "project-01-subnet-1"
+	}
 }
 
 resource aws_subnet subnet2 {
 	vpc_id = aws_vpc.vpc.id
-	availability_zone = ""
+	availability_zone = "ap-northeast-1c"
+	cidr_block = "192.168.1.0/24"
+	tags = {
+		Name = "project-01-subnet-2"
+	}
 }
 
 resource aws_subnet subnet3 {
 	vpc_id = aws_vpc.vpc.id
-	availability_zone = ""
-}
-
-resource aws_subnet subnet4 {
-	vpc_id = aws_vpc.vpc.id
-	availability_zone = ""
+	availability_zone = "ap-northeast-1d"
+	cidr_block = "192.168.2.0/24"
+	tags = {
+		Name = "project-01-subnet-3"
+	}
 }
