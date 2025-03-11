d840a0fe5c2b76d0de0816141fda2ef80085ffb9
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-03-11 23:54:25 +0900

    fix: move instance private address

--- aws/01-alb-to-instances/instance.tf
+++ aws/01-alb-to-instances/instance.tf
@@ -6,7 +6,7 @@ locals {
 resource "aws_instance" "instance1" {
   ami           = local.ami
   subnet_id     = aws_subnet.subnet1.id
-  private_ip    = "192.168.0.1"
+  private_ip    = "192.168.0.5"
   instance_type = local.type
   tags = {
     Name = "project-01-subnet-01-instance-01"
@@ -16,7 +16,7 @@ resource "aws_instance" "instance1" {
 resource "aws_instance" "instance2" {
   ami           = local.ami
   subnet_id     = aws_subnet.subnet2.id
-  private_ip    = "192.168.1.1"
+  private_ip    = "192.168.1.5"
   instance_type = local.type
   tags = {
     Name = "project-01-subnet-02-instance-01"
@@ -26,7 +26,7 @@ resource "aws_instance" "instance2" {
 resource "aws_instance" "instance3" {
   ami           = local.ami
   subnet_id     = aws_subnet.subnet3.id
-  private_ip    = "192.168.2.1"
+  private_ip    = "192.168.2.5"
   instance_type = local.type
   tags = {
     Name = "project-01-subnet-03-instance-01"
