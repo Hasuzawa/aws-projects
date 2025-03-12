157d7a68af02c17807fb195e76c6e2c5a613ebdb
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-03-12 22:38:39 +0900

    feat: add data file as initial data

---
+++ aws/02-dynamo-db/file.tf
@@ -0,0 +1,11 @@
+data local_file hamlet {
+	filename = "./data/hamlet.json"
+}
+
+data local_file les_miserables {
+	filename = "./data/les-miserables.json"
+}
+
+data local_file faust {
+	filename = "./data/faust.json"
+}
