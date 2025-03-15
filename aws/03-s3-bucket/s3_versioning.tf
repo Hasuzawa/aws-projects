6c92b3b8422cb6d5171eb7ef7902632b4e567424
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-03-15 15:35:47 +0900

    feat: add bucket replication

--- aws/03-s3-bucket/s3_versioning.tf
+++ aws/03-s3-bucket/s3_versioning.tf
@@ -1,10 +1,17 @@
-resource "aws_s3_bucket_versioning" "versionings" {
-  for_each = toset([
-    aws_s3_bucket.public_bucket.id,
-    aws_s3_bucket.private_bucket.id
-  ])
-  bucket = each.value
+resource "aws_s3_bucket_versioning" "public_bucket" {
+  bucket = aws_s3_bucket.public_bucket.id
+
   versioning_configuration {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_versioning" "private_bucket" {
+  bucket = aws_s3_bucket.private_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# versioning cannot be deleted nor disabled when there is replication
