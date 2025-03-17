a45c46f31c1110a4c8b79736b0dd1e6eca5b75f5
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-03-17 23:36:26 +0900

    feat: add cloudwatch application insights

--- aws/04-cloud-watch-cloud-trail/cloud_watch.tf
+++ aws/04-cloud-watch-cloud-trail/cloud_watch.tf
@@ -1,1 +1,7 @@
+resource "aws_applicationinsights_application" "ec2" {
+  resource_group_name = aws_resourcegroups_group.ec2.name
+}
 
+resource "aws_applicationinsights_application" "s3" {
+  resource_group_name = aws_resourcegroups_group.s3.name
+}
