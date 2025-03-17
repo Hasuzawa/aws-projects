5353a24efe1e6c544ef884940e182e56a85bbd5f
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-03-18 00:08:29 +0900

    feat: add comment for application insights

--- aws/04-cloud-watch-cloud-trail/cloud_watch.tf
+++ aws/04-cloud-watch-cloud-trail/cloud_watch.tf
@@ -1,3 +1,4 @@
+// These will be under CloudWatch > Insights > Application Insights
 resource "aws_applicationinsights_application" "ec2" {
   resource_group_name = aws_resourcegroups_group.ec2.name
 }
