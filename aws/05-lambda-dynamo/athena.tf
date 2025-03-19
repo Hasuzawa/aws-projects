e5b651c7b3f1259b55e3f799d4b491e11d2aff16
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-03-20 00:34:42 +0900

    feat: add athena, s3 files

---
+++ aws/05-lambda-dynamo/athena.tf
@@ -0,0 +1,24 @@
+locals {
+  workgroup = replace("${var.project_name}-dev-workgroup", "-", "_")
+  database  = replace("${var.project_name}-dev-database", "-", "_")
+}
+
+# default workgroup is called primary
+resource "aws_athena_workgroup" "dev" {
+  name        = local.workgroup
+  description = "dev workspace for project-05"
+
+  configuration {
+    bytes_scanned_cutoff_per_query = 10 * 1024 * 1024
+
+    result_configuration {
+      output_location = "s3://${aws_s3_bucket.bucket.bucket}"
+    }
+  }
+}
+
+# this will belong to the default 'None' catalog
+resource "aws_athena_database" "dev" {
+  name   = local.database
+  bucket = aws_s3_bucket.bucket.id
+}
