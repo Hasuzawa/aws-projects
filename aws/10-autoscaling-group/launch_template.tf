6944dbc27f545ed9ab087ab0775fa86e82d1115e
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-03-28 00:52:51 +0900

    feat: add launch template

---
+++ aws/10-autoscaling-group/launch_template.tf
@@ -0,0 +1,52 @@
+resource "aws_launch_template" "zero" {
+  name        = "zero-config"
+  description = "template with all default values"
+}
+
+resource "aws_launch_template" "t2micro" {
+  name          = "simple-t2-micro"
+  description   = "very simple setup"
+  instance_type = "t2.micro"
+}
+
+resource "aws_launch_template" "customized" {
+  name          = "customized"
+  description   = "highly customized template"
+  instance_type = "t3.micro"
+
+  cpu_options {
+    # these depends on instance type
+    core_count       = 2
+    threads_per_core = 2
+  }
+
+  enclave_options {
+    enabled = true
+  }
+
+  hibernation_options {
+    configured = true
+  }
+
+  instance_initiated_shutdown_behavior = "stop" # default, can choose terminate
+
+  instance_market_options {
+    # market_type = "spot"
+    # spot_options {
+    # 	max_price = "0.0001"
+    # }
+  }
+
+  maintenance_options {
+  }
+
+  metadata_options {
+  }
+  network_interfaces {
+  }
+  placement {
+    affinity = "" # only relevant when on dedicated host
+    # availability_zone = ""
+    # partition_number = 0 # only relevant for group strategy partition
+  }
+}
