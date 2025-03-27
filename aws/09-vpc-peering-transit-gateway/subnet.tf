0f81e86f6dc97ac2c7788ce61014466609a6f006
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-03-27 22:44:58 +0900

    feat: use route table for subnets

--- aws/09-vpc-peering-transit-gateway/subnet.tf
+++ aws/09-vpc-peering-transit-gateway/subnet.tf
@@ -7,3 +7,17 @@ resource "aws_subnet" "subnets" {
     Name = "subnet-${var.project_name}-${count.index}"
   }
 }
+
+locals {
+  route_table_ids = [
+    aws_route_table.rt0,
+    aws_route_table.rt1,
+    aws_route_table.rt2
+  ]
+}
+
+resource aws_route_table_association asso {
+  count = length(aws_subnet.subnets)
+  subnet_id = aws_subnet.subnets[count.index].id
+  route_table_id = local.route_table_ids[count.index].id
+}
