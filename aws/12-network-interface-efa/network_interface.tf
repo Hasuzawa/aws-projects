7caa560acdf0567d00954cb1a45d47dea6b87bd3
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-03-30 18:00:57 +0900

    feat: add more network interface types

--- aws/12-network-interface-efa/network_interface.tf
+++ aws/12-network-interface-efa/network_interface.tf
@@ -1,8 +1,31 @@
 resource "aws_network_interface" "standard" {
-  subnet_id = aws_subnet.subnet.id
-  interface_type = "interface"
+  subnet_id   = aws_subnet.subnet.id
   description = "standard network interface"
   tags = {
-	Name = "eni-${var.project_name}"
+    Name = "eni-${var.project_name}"
+  }
+}
+
+resource "aws_network_interface" "trunk" {
+  subnet_id      = aws_subnet.subnet.id
+  interface_type = "trunk"
+  tags = {
+    Name = "trunk-${var.project_name}"
+  }
+}
+
+resource "aws_network_interface" "efa" {
+  subnet_id      = aws_subnet.subnet.id
+  interface_type = "efa"
+  tags = {
+    Name = "efa-${var.project_name}"
+  }
+}
+
+resource "aws_network_interface" "efa" {
+  subnet_id      = aws_subnet.subnet.id
+  interface_type = "efa-only"
+  tags = {
+    Name = "efa-only-${var.project_name}"
   }
 }
