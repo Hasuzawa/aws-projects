1eeb98b9b0555b46f500540f1699929f8aba9131
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-03-31 00:55:40 +0900

    feat: add instances connected with efa

---
+++ aws/12-network-interface-efa/instances_connected_with_efa.tf
@@ -0,0 +1,42 @@
+# two instances with EFA interface. Only some instance types support EFA.
+
+# only the more powerful instances support EFA
+locals {
+  instance_type = "c5n.9xlarge"
+}
+
+resource "aws_instance" "efa_instance_0" {
+  ami           = "ami-05506fa68391b4cb1"
+  instance_type = local.instance_type
+  network_interface {
+    network_interface_id = aws_network_interface.standard[3].id
+    device_index         = 0
+    network_card_index   = 0
+  }
+  network_interface {
+    network_interface_id = aws_network_interface.efa_only.id
+    device_index         = 1
+    network_card_index   = 0
+  }
+  tags = {
+    Name = "instance-with-efa-${var.project_name}-0"
+  }
+}
+
+resource "aws_instance" "efa_instance_1" {
+  ami           = "ami-05506fa68391b4cb1"
+  instance_type = local.instance_type
+  network_interface {
+    network_interface_id = aws_network_interface.standard[4].id
+    device_index         = 0
+    network_card_index   = 0
+  }
+  network_interface {
+    network_interface_id = aws_network_interface.efa_only.id
+    device_index         = 1
+    network_card_index   = 0
+  }
+  tags = {
+    Name = "instance-with-efa-${var.project_name}-1"
+  }
+}
