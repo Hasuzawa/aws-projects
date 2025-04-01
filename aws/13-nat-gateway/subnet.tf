83aae4846df71ca6bed62e7c5274c2d3e57fbc4c
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-04-01 22:03:48 +0900

    feat: split subnet into private & public

--- aws/13-nat-gateway/subnet.tf
+++ aws/13-nat-gateway/subnet.tf
@@ -6,12 +6,21 @@ data "aws_availability_zones" "available" {
   }
 }
 
-resource "aws_subnet" "subnets" {
-  count             = 3
+resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.vpc.id
-  cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 4, count.index)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 4, 0)
+  availability_zone = data.aws_availability_zones.available.names[0]
   tags = {
-    Name = "subnet-${var.project_name}-${count.index}"
+    Name = "subnet-public-${var.project_name}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 4, 1 + count.index)
+  availability_zone = data.aws_availability_zones.available.names[1 + count.index]
+  tags = {
+    Name = "subnet-private-${var.project_name}-${count.index}"
   }
 }
