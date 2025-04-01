3b50e34687233291d4d7c7818d0f023fb915d48c
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-04-01 23:34:15 +0900

    feat: remove commented out code for nat gateway

--- aws/13-nat-gateway/nat_gateway.tf
+++ aws/13-nat-gateway/nat_gateway.tf
@@ -6,12 +6,3 @@ resource "aws_nat_gateway" "public_ngw" {
     Name = "ngw-public"
   }
 }
-
-# resource "aws_nat_gateway" "private_ngws" {
-#   count             = 2
-#   subnet_id         = aws_subnet.subnets[count.index + 1].id
-#   connectivity_type = "private"
-#   tags = {
-#     Name = "ngw-private-${count.index}"
-#   }
-# }
