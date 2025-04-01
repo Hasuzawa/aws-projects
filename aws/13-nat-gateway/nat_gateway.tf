c7140816dcbce2a6cabad4e9f9655bb0860b41bf
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-04-01 22:04:56 +0900

    feat: add public nat gateway

--- aws/13-nat-gateway/nat_gateway.tf
+++ aws/13-nat-gateway/nat_gateway.tf
@@ -1,17 +1,17 @@
 resource "aws_nat_gateway" "public_ngw" {
-  subnet_id         = aws_subnet.subnets[0].id
+  subnet_id         = aws_subnet.public.id
   connectivity_type = "public"
   allocation_id     = aws_eip.eip.allocation_id
   tags = {
-    Name = "ngw-0"
+    Name = "ngw-public"
   }
 }
 
-resource "aws_nat_gateway" "private_ngws" {
-  count             = 2
-  subnet_id         = aws_subnet.subnets[count.index + 1].id
-  connectivity_type = "private"
-  tags = {
-    Name = "ngw-${count.index + 1}"
-  }
-}
+# resource "aws_nat_gateway" "private_ngws" {
+#   count             = 2
+#   subnet_id         = aws_subnet.subnets[count.index + 1].id
+#   connectivity_type = "private"
+#   tags = {
+#     Name = "ngw-private-${count.index}"
+#   }
+# }
