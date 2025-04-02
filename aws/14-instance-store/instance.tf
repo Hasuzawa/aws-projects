9156f163c4724f05672e3df5628b6920571b7820
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-04-03 01:15:19 +0900

    feat: add instances

---
+++ aws/14-instance-store/instance.tf
@@ -0,0 +1,37 @@
+resource "aws_instance" "instance" {
+  ami           = "ami-05506fa68391b4cb1"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.subnet.id
+  tags = {
+    Name = "instance-with-attached-volume"
+  }
+}
+
+# the instance type must support multi-attach EBS
+locals {
+  sharable_type = "t3.micro"
+}
+
+resource "aws_instance" "instance_1" {
+  ami           = "ami-05506fa68391b4cb1"
+  instance_type = local.sharable_type
+  subnet_id     = aws_subnet.subnet.id
+  #   ephemeral_block_device {
+  #     device_name = ""
+  #   }
+  tags = {
+    Name = "instance-shared-mount-0"
+  }
+}
+
+resource "aws_instance" "instance_2" {
+  ami           = "ami-05506fa68391b4cb1"
+  instance_type = local.sharable_type
+  subnet_id     = aws_subnet.subnet.id
+  #   ephemeral_block_device {
+  #     device_name = ""
+  #   }
+  tags = {
+    Name = "instance-shared-mount-1"
+  }
+}
