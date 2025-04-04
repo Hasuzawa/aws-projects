0f5ac9da09448aa5d54f23ba49123ffc855e88f6
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-04-05 01:17:44 +0900

    feat: move the org account to other OU

--- aws/15-organization/organization_account.tf
+++ aws/15-organization/organization_account.tf
@@ -1,11 +1,13 @@
 resource "aws_organizations_account" "jack" {
   name              = "jack"
   email             = "jack@aws.email.com"
+  parent_id         = aws_organizations_organizational_unit.hardware.id
   close_on_deletion = true
 }
 
 resource "aws_organizations_account" "mary" {
   name              = "mary"
   email             = "mary@aws.email.com"
+  parent_id         = aws_organizations_organizational_unit.software.id
   close_on_deletion = true
 }
