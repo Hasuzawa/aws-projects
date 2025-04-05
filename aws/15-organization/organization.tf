1757264fe052164165301284e59549e21cddf410
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-04-05 13:44:12 +0900

    feat: add organization

--- aws/15-organization/organization.tf
+++ aws/15-organization/organization.tf
@@ -1,15 +1,3 @@
-# resource "aws_organizations_organization" "all_feature" {
-#   feature_set = "ALL"
-# }
-
-# resource "aws_organizations_organization" "consolidated" {
-#   feature_set = "CONSOLIDATED_BILLING"
-# }
-
 # the orgnization of the AWS user
 data "aws_organizations_organization" "current" {
 }
-
-
-
-
