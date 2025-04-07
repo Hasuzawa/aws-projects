173063f852202bbdce54ece2275369ccfa6e29e7
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-04-07 22:17:23 +0900

    feat: add sqs used as pipe src and dst

--- aws/16-event-bridge/sqs.tf
+++ aws/16-event-bridge/sqs.tf
@@ -3,3 +3,15 @@ resource "aws_sqs_queue" "demo" {
   name                      = "event-bridge-downstream"
   message_retention_seconds = 60 * 60 * 72
 }
+
+
+resource "aws_sqs_queue" "source" {
+  name                      = "pipe-source"
+  message_retention_seconds = 3600
+}
+
+resource "aws_sqs_queue" "destination" {
+  name                      = "pipe-destination.fifo"
+  fifo_queue                = true
+  message_retention_seconds = 3600
+}
