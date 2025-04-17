340470d26fea98356b502099ed523dd4a091f9b3
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-04-17 21:51:48 +0900

    feat: add template for stack parameter and output

--- aws/20-cloud-formation/stack.tf
+++ aws/20-cloud-formation/stack.tf
@@ -8,3 +8,18 @@ resource "aws_cloudformation_stack" "simple_vpc" {
   on_failure         = "DO_NOTHING"
   timeout_in_minutes = 5
 }
+
+data "local_file" "using_parameter" {
+  filename = "template/using-parameter-and-output.yml"
+}
+
+resource "aws_cloudformation_stack" "parameter_and_output" {
+  name          = "parameter-and-output"
+  template_body = data.local_file.using_parameter.content
+  parameters = {
+    Name   = "Benjamin"
+    Season = "Winter"
+    Coin   = 13
+  }
+  on_failure = "ROLLBACK"
+}
