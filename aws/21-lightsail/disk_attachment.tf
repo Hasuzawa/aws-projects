80ab08e1827cf8da20512a513a2295bbeeff542c
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-04-20 17:49:27 +0900

    chore: change naming for instance & disk attachment

--- aws/21-lightsail/disk_attachment.tf
+++ aws/21-lightsail/disk_attachment.tf
@@ -1,10 +1,10 @@
-resource "aws_lightsail_disk_attachment" "attachment_1" {
+resource "aws_lightsail_disk_attachment" "a1" {
   disk_name     = aws_lightsail_disk.d1.name
   instance_name = aws_lightsail_instance.i1.name
   disk_path     = "/dev/xvdf"
 }
 
-resource "aws_lightsail_disk_attachment" "attachment_2" {
+resource "aws_lightsail_disk_attachment" "a2" {
   disk_name     = aws_lightsail_disk.d2.name
   instance_name = aws_lightsail_instance.i2.name
   disk_path     = "/dev/xcdg"
--- aws/21-lightsail/instance.tf
+++ aws/21-lightsail/instance.tf
@@ -1,12 +1,12 @@
 resource "aws_lightsail_instance" "i1" {
-  name              = "nginx-instance"
+  name              = "instance-1"
   availability_zone = "ap-northeast-1a"
   blueprint_id      = "nginx"
   bundle_id         = "micro_3_0"
 }
 
 resource "aws_lightsail_instance" "i2" {
-  name              = "nginx-instance"
+  name              = "instance-2"
   availability_zone = "ap-northeast-1c"
   blueprint_id      = "nginx"
   bundle_id         = "micro_3_0"
