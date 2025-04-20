ebc9c2ce6f598fd9831897974e7b4c2c20bd5831
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-04-20 18:05:30 +0900

    fix: change disk to min 8GB size, fix disk path

--- aws/21-lightsail/disk.tf
+++ aws/21-lightsail/disk.tf
@@ -1,11 +1,11 @@
 resource "aws_lightsail_disk" "d1" {
   name              = "disk-1"
-  size_in_gb        = 2
+  size_in_gb        = 8
   availability_zone = "ap-northeast-1a"
 }
 
 resource "aws_lightsail_disk" "d2" {
   name              = "disk-2"
-  size_in_gb        = 4
+  size_in_gb        = 8
   availability_zone = "ap-northeast-1c"
 }
--- aws/21-lightsail/disk_attachment.tf
+++ aws/21-lightsail/disk_attachment.tf
@@ -7,5 +7,5 @@ resource "aws_lightsail_disk_attachment" "a1" {
 resource "aws_lightsail_disk_attachment" "a2" {
   disk_name     = aws_lightsail_disk.d2.name
   instance_name = aws_lightsail_instance.i2.name
-  disk_path     = "/dev/xcdg"
+  disk_path     = "/dev/xvdg"
 }
