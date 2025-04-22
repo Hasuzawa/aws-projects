d51df44b9765640a568549990888f5e0516cb89d
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-04-22 23:42:37 +0900

    feat: add ecs cluster

---
+++ aws/23-container-fargate/cluster.tf
@@ -0,0 +1,18 @@
+resource "aws_ecs_cluster" "fargate" {
+  name = "fargate-cluster"
+}
+
+resource "aws_ecs_cluster_capacity_providers" "fargate" {
+  cluster_name       = aws_ecs_cluster.fargate.name
+  capacity_providers = ["FARGATE"]
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE"
+    base              = 0
+    weight            = 100 # all
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "spot" {
+  cluster_name       = aws_ecs_cluster.fargate.name
+  capacity_providers = ["FARGATE_SPOT"]
+}
