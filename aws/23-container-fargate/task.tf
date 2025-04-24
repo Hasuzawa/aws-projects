faea30f845d51945202a1377c5a232f80112ce1d
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-04-25 00:46:20 +0900

    fix: change to proper ecs task defn

--- aws/23-container-fargate/task.tf
+++ aws/23-container-fargate/task.tf
@@ -1,14 +1,18 @@
 resource "aws_ecs_task_definition" "nginx" {
-  family                = "nginx-task"
-  container_definitions = file("task/nginx.json")
+  family                   = "nginx-task"
+  container_definitions    = file("task/nginx.json")
+  cpu                      = "0.5 vCPU"
+  memory                   = "1 GB"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
 }
 
 resource "aws_ecs_task_definition" "multi_nginx" {
   family                = "multi-nginx-task"
   container_definitions = file("task/multi-nginx.json")
   # these two are task cpu and memory, not container cpu and memory
-  cpu    = "1vcpu"
-  memory = "2GB"
+  cpu    = "1 vCPU"
+  memory = "2 GB"
 
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
