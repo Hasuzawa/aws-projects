239a2611c361dca31ca1807b085f30dd8059e193
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-04-24 22:40:36 +0900

    feat: update container task definition

--- aws/23-container-fargate/task.tf
+++ aws/23-container-fargate/task.tf
@@ -1,8 +1,19 @@
-data "local_file" "nginx_task" {
-  filename = "./task/nginx.json"
-}
-
 resource "aws_ecs_task_definition" "nginx" {
   family                = "nginx-task"
-  container_definitions = data.local_file.nginx_task.content
+  container_definitions = file("task/nginx.json")
+}
+
+resource "aws_ecs_task_definition" "multi_nginx" {
+  family                = "multi-nginx-task"
+  container_definitions = file("task/multi-nginx.json")
+  # these two are task cpu and memory, not container cpu and memory
+  cpu    = "1vcpu"
+  memory = "2GB"
+
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  runtime_platform {
+    cpu_architecture        = "X86_64"
+    operating_system_family = "LINUX"
+  }
 }
