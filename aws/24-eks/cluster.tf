fbd39dcb9805fb8ef58412f545afeccfcedec0a3
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-04-27 03:09:58 +0900

    feat: give roles to cluster & node

--- aws/24-eks/cluster.tf
+++ aws/24-eks/cluster.tf
@@ -1,6 +1,8 @@
 resource "aws_eks_cluster" "sample" {
   name     = "sample"
-  role_arn = ""
+  version  = "1.32"
+  role_arn = data.aws_iam_role.eks_auto_cluster.arn
+
   vpc_config {
     subnet_ids = data.aws_subnets.subnets.ids
   }
--- aws/24-eks/node.tf
+++ aws/24-eks/node.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_node_group" "nginx_nodes" {
   cluster_name    = aws_eks_cluster.sample.name
   node_group_name = "nginx-group"
-  node_role_arn   = ""
+  node_role_arn   = data.aws_iam_role.eks_auto_node.arn
   subnet_ids      = data.aws_subnets.subnets.ids
   scaling_config {
     desired_size = 1
