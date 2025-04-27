f1c83e738a55388008e10e632deca8275c0d6821
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-04-27 22:40:25 +0900

    feat: fix cluster name

--- aws/24-eks/node.tf
+++ aws/24-eks/node.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_node_group" "nginx_nodes" {
-  cluster_name    = aws_eks_cluster.sample.name
+  cluster_name    = aws_eks_cluster.sample_2.name
   node_group_name = "nginx-group"
   node_role_arn   = data.aws_iam_role.eks_auto_node.arn
   subnet_ids      = data.aws_subnets.subnets.ids
