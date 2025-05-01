56e9fecfc7c8be9b1ae237c03161c1f48d66dae5
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-05-01 20:02:31 +0900

    feat: add weighted routing records

--- aws/25-route-53/record.tf
+++ aws/25-route-53/record.tf
@@ -23,3 +23,29 @@ resource "aws_route53_record" "cname" {
   ttl     = 3600 * 6
   records = ["bedroom.com"]
 }
+
+# weighted routing for same domain name
+
+resource "aws_route53_record" "toilet_1" {
+  name           = "toilet"
+  type           = "A"
+  zone_id        = aws_route53_zone.private.id
+  ttl            = 3600 * 6
+  records        = ["192.168.0.7"]
+  set_identifier = "toilet-weighted-1"
+  weighted_routing_policy {
+    weight = 50
+  }
+}
+
+resource "aws_route53_record" "toilet_2" {
+  name           = "toilet"
+  type           = "A"
+  zone_id        = aws_route53_zone.private.id
+  ttl            = 3600 * 6
+  records        = ["192.168.0.9"]
+  set_identifier = "toilet-weighted-2"
+  weighted_routing_policy {
+    weight = 50
+  }
+}
