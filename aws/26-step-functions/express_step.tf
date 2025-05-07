c58fe5804fb640f4f361db74301bc4dc6b8878c4
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-05-07 23:14:20 +0900

    feat: add state machine

--- aws/26-step-functions/express_step.tf
+++ aws/26-step-functions/express_step.tf
@@ -1,5 +1,6 @@
 resource "aws_sfn_state_machine" "standard" {
   name       = "express-steps"
   role_arn   = ""
+  type       = "EXPRESS"
   definition = file("steps/express.json")
 }
--- aws/26-step-functions/standard_step.tf
+++ aws/26-step-functions/standard_step.tf
@@ -1,5 +1,6 @@
 resource "aws_sfn_state_machine" "standard" {
   name       = "standard-steps"
   role_arn   = ""
+  type       = "STANDARD"
   definition = file("steps/standard.json")
 }
