930498c12c3d3b0289dc5fa3fb906bc34ed608ac
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2025-05-08 00:04:18 +0900

    feat: use role in step functions

--- aws/26-step-functions/express_step.tf
+++ aws/26-step-functions/express_step.tf
@@ -1,6 +1,6 @@
-resource "aws_sfn_state_machine" "standard" {
+resource "aws_sfn_state_machine" "express" {
   name       = "express-steps"
-  role_arn   = ""
+  role_arn   = data.aws_iam_role.step_function.arn
   type       = "EXPRESS"
   definition = file("steps/express.json")
 }
--- aws/26-step-functions/standard_step.tf
+++ aws/26-step-functions/standard_step.tf
@@ -1,6 +1,6 @@
 resource "aws_sfn_state_machine" "standard" {
   name       = "standard-steps"
-  role_arn   = ""
+  role_arn   = data.aws_iam_role.step_function.arn
   type       = "STANDARD"
   definition = file("steps/standard.json")
 }
